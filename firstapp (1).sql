4e891cfe33407217204d0d6f849fdb6b0d4b0727
Omar Baitenov <obaitenov@bvm.kz>
2023-06-06 14:31:32 +0600

    first

--- firstapp (1).sql
+++ firstapp (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 25 2023 г., 14:51
+-- Время создания: Июн 06 2023 г., 11:30
 -- Версия сервера: 8.0.30
 -- Версия PHP: 8.1.9
 
@@ -41,11 +41,11 @@ CREATE TABLE `cars` (
 --
 
 INSERT INTO `cars` (`id`, `name`, `description`, `price`, `image`, `categoria_id`) VALUES
-(21, 'Mersedes XC', 'ACX', 9000000, 'tiger.jpg', 2),
-(23, 'Camry Corolla', 'Black', 75000000, 'camry-vx70-tmb-550x300.jpg', 12),
-(24, 'Camry X', 'X class', 85000000, 'fOtzw_E5A-Ge7ZniRCMxMv6x_XjI43OihoaNWvBxSubeRe2mGxCCEtZuRQnzhMaM09Htk_r4i-sMdD1e0u-NkvchA9kj.jpg', 12),
-(27, 'Mersedes', 'abc', 9000000, 'cruzak.jpg', 2),
-(29, 'Mersedes', 'asaa', 5000, '5110335115ea3c7285ab65ad1efb1a25.jpg', 2);
+(21, 'Mersedes XC', 'ACX', 100, 'tiger.jpg', 2),
+(23, 'Camry Corolla', 'Black', 90, 'camry-vx70-tmb-550x300.jpg', 12),
+(24, 'Camry X', 'X class', 85, 'fOtzw_E5A-Ge7ZniRCMxMv6x_XjI43OihoaNWvBxSubeRe2mGxCCEtZuRQnzhMaM09Htk_r4i-sMdD1e0u-NkvchA9kj.jpg', 12),
+(27, 'Mersedes', 'abc', 95, 'cruzak.jpg', 2),
+(29, 'Mersedes', 'asaa', 85, '5110335115ea3c7285ab65ad1efb1a25.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -82,6 +82,14 @@ CREATE TABLE `commentary` (
   `car_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Дамп данных таблицы `commentary`
+--
+
+INSERT INTO `commentary` (`id`, `description`, `user_id`, `car_id`) VALUES
+(1, 'Хорошая машина', 1, 21),
+(2, 'Mumtaz', 1, 29);
+
 -- --------------------------------------------------------
 
 --
@@ -104,7 +112,10 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `lastname`, `login`, `password`, `type`) VALUES
 (1, 'omar', 'makishev', 'omarali', '123', 'admin'),
 (2, 'Amir', 'Makishev', 'amirm', '123', NULL),
-(3, 'Aslan', 'Makishev', 'lionas', '123', NULL);
+(3, 'Aslan', 'Makishev', 'lionas', '123', NULL),
+(5, 'Alia', 'Omarova', 'Alia', '$2y$10$9MajdZoiQUwpz0JLk6ZQhuY3MkvZmL0tTUAOfZw/J6/k4UbXE1aji', NULL),
+(6, 'asia', 'Omarova', 'asia', '$2y$10$TjWqMd23J7lvZjaXivplY.08QoSMkHck5RJyoGKEZeJWCo1OHi6HO', NULL),
+(7, 'Camry', 'Alp', 'camry', '$2y$10$7z3jgtXLZoZbxN9j8eVAY.GPXatv5gtb49dOGm4X32K.XlLhvWpjq', NULL);
 
 --
 -- Индексы сохранённых таблиц
@@ -142,25 +153,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT для таблицы `commentary`
 --
 ALTER TABLE `commentary`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
